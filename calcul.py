166034a842618806363e54928c1f52bd2ee9050b
Eliot <eliot.barrabes@gmail.com>
2026-01-20 10:16:49 +0000

    Added python with test workflow

---
+++ calcul.py
@@ -0,0 +1,13 @@
+
+
+def add(a: float, b: float) -> float:
+    return a+b
+
+def substract(a: float, b: float) -> float:
+    return a - b
+
+def multiply(a: float, b: float) -> float:
+    return a * b
+
+def divide(a: float, b: float) -> float:
+    return a / b
